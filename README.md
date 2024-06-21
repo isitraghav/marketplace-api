614fd3b68091eb05e2a50c7f375f4c9aca661554
Raghav Yadav <isitraghav@gmail.com>
2024-06-22 00:54:38 +0530

    added seach and feed (basic implimentation)

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# vercel express templete
-To efficiently set up an Express app with Vercel, use the following boilerplate code.
+# Marketplace-API
--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const cache = require("memory-cache");
 const app = express();
 const cors = require("cors");
 var _ = require("underscore");
+require('dotenv').config()
+
+const createClient = require("@supabase/supabase-js").createClient;
+const supabase = createClient(
+  "https://upfmlmvmdaeuznmishjb.supabase.co",
+  process.env.SUPABASE_KEY
+);
 
 app.use(require("sanitize").middleware);
 app.use(express.json());
@@ -36,26 +43,67 @@ const limiter = rateLimit({
 // Apply the rate limiting middleware to all requests
 app.use(limiter);
 
-app.get("/api/cache", async (req, res) => {
-  let data = req.query;
-  if (_.has(data, "pub")) {
-    cache.del(data.pub);
-  }
-  res.json({});
+app.get("/feed", async (req, res) => {
+  await supabase
+    .from("list")
+    .select("*")
+    .then(({ data, error }) => {
+      if (error) {
+        console.log(error);
+      }
+      res.json(data);
+    });
 });
 
-app.get("/api/clearallcache", async (req, res) => {
-  let data = req.query;
-  if (_.has(data, "pass")) {
-    if (data.pass == "Raghav1979") {
-      res.json(JSON.parse(cache.exportJson()));
-      cache.clear();
-      return;
-    }
+app.post("/list", async (req, res) => {
+  const { name, description, price, owner, ship, image } = req.body;
+  if (!/^[a-zA-Z0-9_ ]*$/.test(name) || !/^[a-zA-Z0-9_ ]*$/.test(description)) {
+    return res.status(400).json({ error: "Query can only contain letters, numbers, spaces, and underscores" });
   }
-  res.json({
-    m: "wrong or no password",
-  });
+  if (name.length < 3 || name.length > 50 || description.length > 500) {
+    return res.status(400).json({ error: "Query must be between 3 and 50 characters long" });
+  }
+  if (name === "" || description === "") {
+    return res.status(400).json({ error: "Name and description cannot be empty" });
+  }
+  await supabase
+    .from("list")
+    .insert({
+      name,
+      description,
+      price,
+      owner,
+      ship,
+      images: [`https://ucarecdn.com/${image}/-/preview/500x500/`],
+    })
+    .then(({ data, error }) => {
+      if (error) {
+        console.log(error);
+      }
+      res.json(data);
+    });
+});
+
+app.get("/search/:name", async (req, res) => {
+  const name = req.params.name;
+  if (!/^[a-zA-Z0-9]*$/.test(name)) {
+    return res.status(400).json({ error: "Query can only contain letters and numbers" });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ error: "Query must be at least 3 characters" });
+  }
+  const { data, error } = await supabase
+    .from("list")
+    .select("*")
+    .ilike("description", `%${name}%`)
+    .ilike("name", `%${name}%`)
+    
+  if (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+
+  res.json(data);
 });
 
 app.listen(process.env.PORT || 3000);
